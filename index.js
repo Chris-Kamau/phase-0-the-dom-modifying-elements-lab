60b7176b8b2fb739cbe2255fa522cb9d4815da3b
Chris <chris.kamau@student.moringaschool.com>
2023-03-22 20:26:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const removeId = document.querySelector('main');
+removeId.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Chris is the champion";
